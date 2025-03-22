e04e5536b4f1f5ec4a2fb7621557e6940f1eefee
Nguyen Khanh <nguyenkhanhit@live.com>
2025-03-22 17:05:10 +0700

    change 22/3/2025

---
+++ APMS/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APMS.Controllers
+{
+    [Authorize()]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
